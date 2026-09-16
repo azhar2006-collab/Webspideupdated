40007883e84c3d841d05fd2a7f65fe1ccc2b9e05
SYED AZHAR HUSSIAN.A <azhar12082006@gmail.com>
2026-09-16 17:10:18 +0530

    Delete blog: "WEB"

--- blogs-data.js
+++ blogs-data.js
@@ -6,7 +6,7 @@
 (function() {
   var blogPayload = {
   "version": "2.0.0",
-  "lastUpdated": "2026-09-16T11:40:04.922Z",
+  "lastUpdated": "2026-09-16T11:40:16.692Z",
   "categories": [
     "Web Development",
     "App Development",

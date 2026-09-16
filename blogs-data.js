620476373e02fe35dfc53d06406ae0fb3b11cba7
SYED AZHAR HUSSIAN.A <azhar12082006@gmail.com>
2026-09-16 13:25:55 +0530

    Update blogs database via Admin Panel [13:25:54]

--- blogs-data.js
+++ blogs-data.js
@@ -6,7 +6,7 @@
 (function() {
   var blogPayload = {
   "version": "2.0.0",
-  "lastUpdated": "2026-09-16T07:54:01.118Z",
+  "lastUpdated": "2026-09-16T07:55:54.134Z",
   "categories": [
     "Web Development",
     "App Development",
